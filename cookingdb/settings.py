a359ebc85f2a673d8aa4d43a89a168e33cca8f86
Constantino Saldana <constantino.saldana@gmail.com>
2021-01-29 15:28:54 -0500

    search queries

--- cookingdb/settings.py
+++ cookingdb/settings.py
@@ -67,6 +67,7 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'https://cookingnuxt.herokuapp.com/'
     )
 
 ROOT_URLCONF = 'cookingdb.urls'
