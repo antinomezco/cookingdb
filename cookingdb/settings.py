d2f16972b5de191254aa6085d6058322cf48b5ca
Constantino Saldana <constantino.saldana@gmail.com>
2021-06-17 14:16:51 -0400

    added whitelist for calaveria

--- cookingdb/settings.py
+++ cookingdb/settings.py
@@ -65,10 +65,11 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://127.0.0.1:3000',
-    'http://127.0.0.1:8080',
-    'http://localhost:3000',
-    'http://localhost:8080',
+    # 'http://127.0.0.1:3000',
+    # 'http://127.0.0.1:8080',
+    # 'http://localhost:3000',
+    # 'http://localhost:8080',
+    'https://agitated-hermann-0d809c.netlify.app/',
     'https://cookingnuxt.herokuapp.com/'
     )
 
